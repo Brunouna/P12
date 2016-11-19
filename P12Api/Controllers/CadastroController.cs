6beab6e6af58a5afaa6c935b6e49ddc3338ed0d7
Bruno <Bruno@DESKTOP-DISLOT6>
2016-11-19 19:32:10 -0200

    Ajuste no cadastro

---
+++ P12Api/Controllers/CadastroController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace P12Api.Controllers
+{
+    public class CadastroController : ApiController
+    {
+        // GET: api/Cadastro
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Cadastro/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Cadastro
+        public string Post([FromBody]Condominos value)
+        {
+            Metodos meusMetodos = new P12Api.Metodos();
+            DataBase db = new DataBase();
+            bool retorno;
+
+            if (value != null)
+            {
+                //VERIFICA E-MAIL
+                if (value.Nome == null || value.Email == null || value.Telefone == null || value.Senha == null)
+                {
+                    return "Favor Preencher todos os campos";
+                }
+
+                retorno = meusMetodos.VerificaEmail(value.Email);
+                if (retorno)
+                {
+                    return "Já existe um E-mail " + value.Email + " cadastrado!";
+                }
+
+                //VERIFICA RESPONSAVEL
+                if (value.Responsavel)
+                {
+                    retorno = meusMetodos.VerificaResponsavel(value.Apartamento);
+                    if (retorno)
+                    {
+                        return "Já existe um responsável cadastrado para o apartamento este apartamento" + value.Apartamento;
+                    }
+                }
+
+                //VERIFICA SINDICO
+                if (value.Sindico)
+                {
+                    retorno = meusMetodos.VerificaSindico();
+                    if (retorno)
+                    {
+                        return "Já existe um sindico cadastrado para o prédio";
+                    }
+                }
+
+                try
+                {
+                    var condominos = value;
+
+                    string insert = "INSERT INTO tblCondominos VALUES('" + condominos.Nome + "', '" + condominos.Telefone + "', '" + condominos.Email + "', '" + condominos.Responsavel + "', '" + condominos.Sindico + "', '" + condominos.Senha + "', " + condominos.Apartamento + ")";
+
+                    db.ExecuteCommand(insert);
+
+                    //UPDATE SE FOR RESPONSAVEL NA TABELA APARTAMENTOS
+                    if (value.Responsavel)
+                    {
+                        int id = meusMetodos.DescobreId(value.Email);
+                        string update = "update tblApartamento set IdCondonimo = " + id + ", Ocupacao = 'true' where Numero = " + value.Apartamento + " ";
+                        db.ExecuteCommand(update);
+                    }
+
+                    return "Cadastrado com sucesso!";
+                }
+                catch (Exception ex)
+                {
+                    return "Erro ao Cadastrar";
+                    //throw;
+                }
+            }
+            else
+            {
+                return "Erro ao Cadastrar";
+            }
+        }
+
+        // PUT: api/Cadastro/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Cadastro/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ P12Api/Metodos.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace P12Api
+{
+    public class Metodos
+    {
+        //VERRIFICA SE EXISTE E-MAIL CADASTRADO
+        public bool VerificaEmail(string Email)
+        {
+            DataBase db = new DataBase();
+            DataSet ds = new DataSet();
+
+            string select = "select Email from tblCondominos where Email = '" + Email + "'";
+
+            ds = db.GetDataSet(select);
+
+            if (ds.Tables[0].Rows.Count != 0)
+            {
+                return ds.Tables[0].Rows[0].ItemArray.Any();
+            }
+
+            return false;
+        }
+
+        public bool VerificaResponsavel(string apartamento)
+        {
+            DataBase db = new DataBase();
+            DataSet ds = new DataSet();
+            string select = "select Responsavel from tblCondominos where Apartamento = " + apartamento + " and Responsavel = 'true'";
+
+            ds = db.GetDataSet(select);
+
+            if (ds.Tables[0].Rows.Count != 0)
+            {
+                return ds.Tables[0].Rows[0].ItemArray.Any();
+            }
+
+            return false;
+        }
+
+        public bool VerificaSindico()
+        {
+            DataBase db = new DataBase();
+            DataSet ds = new DataSet();
+            string select = "select sindico from tblCondominos where sindico = 'true'";
+
+            ds = db.GetDataSet(select);
+
+            if (ds.Tables[0].Rows.Count != 0)
+            {
+                return ds.Tables[0].Rows[0].ItemArray.Any();
+            }
+
+            return false;
+        }
+
+        public int DescobreId(string email)
+        {
+            DataBase db = new DataBase();
+            DataSet ds = new DataSet();
+            int retorno = 0;
+            string select = "select id from tblCondominos where Email = '" + email + "'";
+
+            ds = db.GetDataSet(select);
+
+            if (ds.Tables[0].Rows.Count != 0)
+            {
+                retorno = (int)ds.Tables[0].Rows[0].ItemArray.ElementAt(0);
+            }
+
+            return retorno;
+        }
+    }
+}
